ad88168fab5ca54bd0eb939f56d7faa15f560874
RIYAZ ALAM <riyazrafique06@gmail.com>
2026-03-17 20:59:53 +0530

    set 6 digit code

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -13,10 +13,12 @@ const register = async (req, res) => {
             return res.status(400).json({success:false, message:"User already exists , Please login"});
         }
         const hashedPassword = await bcryptjs.hashSync(password, 10);
+        const verificationCode = Math.floor(100000 + Math.random() * 900000).toString(); //to generate 6 digit random number as verification code for every user
         const user = new UserModel({
             name, 
             email,
-            password: hashedPassword
+            password: hashedPassword,
+            verificationCode
         })
         await user.save();
         return res.status(200).json({success:true , message:"User registered successfully",user});
